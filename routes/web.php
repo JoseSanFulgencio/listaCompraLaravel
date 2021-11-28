cbecd1bcf8fda71b371b8b5e736c1aa8b34a7590
JoseSanFulgencio <alumno@vm1>
2021-11-28 22:16:25 +0100

    Esqueleto y primeras vistas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('login', function () {
+    return view ('auth.login');
+});
+
+Route::get('logout', function () {
+    return ('Logout usuario');
+});
+
+Route::get('productos', function () {
+    return view ('productos.index');
+});
+
+Route::get('productos/show/{id}', function () {
+    return view ('productos.show', array('id'=>$id));
+});
+
+Route::get('productos/create', function () {
+    return view ('productos.create');
+});
+
+Route::get('productos/edit/{id}', function () {
+    return view ('productos.edit', array('id'=>$id));
 });
