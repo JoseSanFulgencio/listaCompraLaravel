8c3bdb5119837028dcde0742d0f7948d79713852
JoseSanFulgencio <alumno@vm1>
2021-12-03 02:31:05 +0100

    Base de datos, migraciones y seeds

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -8,38 +8,34 @@
 
     <div class="col-sm-4">
 
-        <a href="{{ url('/productos/show/' . $id ) }}">
-            <img src="" style="height:200px"/>
+        <a href="{{ url('/productos/show/' . $producto->id ) }}">
+            <img src="{{$producto->imagen}}" style="height:200px"/>
         </a>
 
     </div>
     <div class="col-sm-8">
 
-        <h4>{{$producto[0]}}</h4>
-        <h6>Categoría: {{$producto[1]}}</h6>
+        <h4>{{$producto->nombre}}</h4>
+        <h6>Categoría: {{$producto->categoria}}</h6>
         <p><strong>Estado: </strong>
-            @if($producto)
+            @if($producto->pendiente)
                 Producto actualmente comprado.
             @else
                 Producto en stock.
             @endif
         </p>
 
-        @if($producto)
-            <a class="btn btn-danger" href="#">Devolver producto</a>
+        @if($producto->pendiente)
+            <a class="btn btn-danger" href="#">Comprado</a>
         @else
             <a class="btn btn-primary" href="#">Comprar producto</a>
         @endif
-        <a class="btn btn-warning" href="{{ url('/productos/edit/' . $id ) }}">
+        <a class="btn btn-warning" href="{{ url('/productos/edit/' . $producto->id ) }}">
             <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
             Editar producto</a>
         <a class="btn btn-outline-info" href="{{ action('App\Http\Controllers\ProductoController@getIndex') }}">Volver al listado</a>
 
     </div>
 </div>
-
-@stop
-
-@stop
 
 @stop
